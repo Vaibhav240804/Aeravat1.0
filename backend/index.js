281927abf2f1ab484156dd10bd0df0be3353fe81
AdityaGangal22 <119730601+AdityaGangal22@users.noreply.github.com>
2024-03-23 13:56:15 +0530

    Updated blog api

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const uR = require("./routers/userRouter.js");
 const app = express();
 const port = 3000;
+const axios = require('axios');
+
 
 const bR = express.Router();
 app.use("/api", bR);
@@ -33,6 +35,26 @@ app.post("/sms", (req, res) => {
     res.json({ prediction: prediction.trim() });
   });
 });
+
+app.get('/news', async (req, res) => {
+  const options = {
+      method: 'GET',
+      url: 'https://reuters-business-and-financial-news.p.rapidapi.com/article-date/2024-01-01/0/20',
+      headers: {
+          'X-RapidAPI-Key': 'befc06d1b8msh6f066a8550ac960p15a686jsndc5ed0af9d9e',
+          'X-RapidAPI-Host': 'reuters-business-and-financial-news.p.rapidapi.com'
+      }
+  };
+
+  try {
+      const response = await axios.request(options);
+      res.json(response.data);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while fetching data from Reuters API' });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
